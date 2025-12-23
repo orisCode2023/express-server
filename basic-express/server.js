3ae1e22da8ba7ca371f9440df11b7089dcf39f03
odmark2023@gmail.com <odmark2023@gmail.com>
2025-12-23 15:55:33 +0200

    finish basic server

--- basic-express/server.js
+++ basic-express/server.js
@@ -22,21 +22,21 @@ async function readJson(path) {
     return JSON.parse(await fs.readFile(path, 'utf-8'));
 };
 
-async function writeJson(path, data){
+async function writeJson(path, data) {
     await fs.writeFile(path, JSON.stringify(data, null, 2), 'utf-8');
 };
 
 const getNextId = (users) => {
-  if (!USERS_DATA || USERS_DATA.length === 0) {
-    return 1;
-  }
-  let maxValue = 0;
-  users.forEach((user) => {
-    if (user.id > maxValue) {
-      maxValue = user.id;
+    if (!USERS_DATA || USERS_DATA.length === 0) {
+        return 1;
     }
-  });
-  return maxValue + 1;
+    let maxValue = 0;
+    users.forEach((user) => {
+        if (user.id > maxValue) {
+            maxValue = user.id;
+        }
+    });
+    return maxValue + 1;
 };
 
 app.get('/users', async (req, res) => {
@@ -45,14 +45,15 @@ app.get('/users', async (req, res) => {
         else res.status(200).json({ msg: "success", result: USERS_DATA });
     } catch (err) {
         console.error(err);
-}});
+    }
+});
 
 app.get('/users/search', async (req, res) => {
     try {
         const { city } = req.query;
         const filteredUsers = USERS_DATA.filter(userObg => userObg.city === city)
-        if(filteredUsers.length > 0) res.status(200).json(filteredUsers)
-        else res.status(404).json({ message: "users not found with this city"})
+        if (filteredUsers.length > 0) res.status(200).json(filteredUsers)
+        else res.status(404).json({ message: "users not found with this city" })
     } catch (err) {
         console.error(err)
     }
@@ -72,20 +73,43 @@ app.post('/users', async (req, res) => {
         age: req.body.age || 0,
         city: req.body.city || null,
     }
-    USERS_DATA.push(newUser) 
+    USERS_DATA.push(newUser)
     await writeJson(PATH, USERS_DATA)
-    res.status(201).json({msg: "new user was created succefully", data: newUser})
+    res.status(201).json({ msg: "new user was created succefully", data: newUser })
 });
 
 app.put('/users/:id', async (req, res) => {
     const id = req.params.id
-    const foundUser = USERS_DATA.find(user => user.id === id)
-    foundUser["id"] = req.body.id
-    await writeJson(PATH, newUser)
+    const intId = parseInt(id)
+    const userToUpdate = USERS_DATA.find(user => user.id === intId)
+    if (isNaN(intId)) {
+        throw new Error("Invalid user id ")
+    }
+    else if (!userToUpdate) {
+        res.status(404).json({ msg: "user not found" })
+    }
+    else {
+        userToUpdate.name = req.body.name || userToUpdate.name
+        userToUpdate.age = req.body.age || userToUpdate.age
+        userToUpdate.city = req.body.city || userToUpdate.city
+        // userToUpdate = {id:intId,...req.body}????? 
+        await writeJson(PATH, USERS_DATA)
+        res.status(200).json({ success: true, data: userToUpdate });
+    }
 
+
+});
+
+app.delete('/users/:id', async (req, res) => {
+    const id = req.body.id
+    const newData = USERS_DATA.filter(user => user.id !== id)
+    await writeJson(PATH, newData)
+    if (newData.length === USERS_DATA.length){
+        res.status(404).json({msg: "user not found"})
+    } else {
+        res.status(200).json({msg: 'deleted completly'})
+    }
 })
-
-
 
 
 
