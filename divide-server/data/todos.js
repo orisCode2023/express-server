988cf42aa69d5cc7906db0df6f9dfdb83757c1f6
odmark2023@gmail.com <odmark2023@gmail.com>
2025-12-23 12:02:47 +0200

    the server divide to a functions and folders

---
+++ divide-server/data/todos.js
@@ -0,0 +1,26 @@
+[
+  {
+    "id": 2,
+    "title": "Throw garbage",
+    "description": "start now",
+    "completed": false,
+    "created_at": "2025-12-18T22:50:02.030Z",
+    "updated_at": "2025-12-18T22:50:02.030Z"
+  },
+  {
+    "title": "Walk the dog",
+    "description": "start now",
+    "completed": false,
+    "id": 4,
+    "created_at": "2025-12-21T10:01:31.189Z",
+    "updated_at": "2025-12-21T10:01:31.189Z"
+  },
+  {
+    "title": "Walk the dog!!!",
+    "description": "start now",
+    "completed": false,
+    "id": 5,
+    "created_at": "2025-12-23T08:21:15.196Z",
+    "updated_at": "2025-12-23T08:21:15.196Z"
+  }
+]
